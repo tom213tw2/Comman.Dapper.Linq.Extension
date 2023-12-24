193a77a26a5802bc1b6ec36794859d493d229763
高宗毅 <b98170005@gmail.com>
2023-12-25 00:02:51 +0800

    Implementing a Cool MySql Class

---
+++ Comman.Dapper.Linq.Extension/Comman.Dapper.Linq.MySql/DapperRepository.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+using Comman.Dapper.Linq.Extension.Core.Interfaces;
+using Comman.Dapper.Linq.Extension.Dapper;
+using Comman.Dapper.Linq.Extension.Extension.From;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Comman.Dapper.Linq.MySql
+{
+    public class DapperRepository<T> where T:IBaseEntity
+    {
+        private readonly IDbTransaction _transaction;
+        private readonly IDbConnection _connection;
+        public IQuerySet<T> Query { get; private set; }
+        public ICommandSet<T> Command { get;private set; }
+
+        public DapperRepository(IDbConnection connection)
+        {
+            this._connection = connection;
+            Query = _connection.QuerySet<T>();
+            Command = _connection.CommandSet<T>();
+        }
+
+        public DapperRepository(IDbConnection connection, IDbTransaction transaction)
+        {
+            this._transaction = transaction;
+            this._connection = connection;
+            Query = _connection.QuerySet<T>(_transaction);
+            Command = _connection.CommandSet<T>(_transaction);
+        }
+        
+        public string QuerySqlString()
+        {
+            return GetSqlString(Query);
+        }
+
+
+
+        public string CommandSqlString()
+        {
+            return GetSqlString(Command);
+        }
+
+        private string GetSqlString(object queryOrCommand)
+        {
+            try
+            {
+                var sqlProviderField = queryOrCommand.GetType().GetField("SqlProvider");
+                if (sqlProviderField == null)
+                {
+                    throw new InvalidOperationException("Cannot find the SqlProvider field.");
+                }
+
+                var sqlProvider = (MySqlProvider)sqlProviderField.GetValue(queryOrCommand);
+                return GetParametersWithValues(sqlProvider.Params, sqlProvider.SqlString);
+            }
+            catch (System.Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private string GetParametersWithValues(DynamicParameters sqlProviderParams, string sqlProviderSqlString)
+        {
+            StringBuilder sb = new StringBuilder(sqlProviderSqlString + ";");
+
+            var dic = ObjectToDictionary(sqlProviderParams.GetType().GetField("parameters", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(sqlProviderParams));
+            foreach (var item in dic.Values)
+            {
+                sb.AppendLine();
+                sb.AppendFormat("{0} : {1}", item["Name"], item["Value"]);
+            }
+
+            return sb.ToString();
+        }
+
+        private Dictionary<string, JObject> ObjectToDictionary(object obj) => JsonConvert.DeserializeObject<Dictionary<string, JObject>>(JsonConvert.SerializeObject(obj));
+    }
+}
---
+++ Comman.Dapper.Linq.Extension/Comman.Dapper.Linq.MySql/ResolveExpression.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Comman.Dapper.Linq.Extension;
+using Comman.Dapper.Linq.Extension.Core.Interfaces;
+using Comman.Dapper.Linq.Extension.Entites;
+using Comman.Dapper.Linq.Extension.Exception;
+using Comman.Dapper.Linq.Extension.Expressions;
+using Comman.Dapper.Linq.Extension.Extension;
+using Comman.Dapper.Linq.Extension.Helper.Cache;
+
+namespace Comman.Dapper.Linq.MySql
+{
+    internal class ResolveExpression : IResolveExpression
+    {
+        public ResolveExpression(SqlProvider provider) : base(provider)
+        {
+
+        }
+
+        public override string ResolveSelect(int? topNum)
+        {
+            // 添加需要連接的導航表
+            var masterEntity = EntityCache.QueryEntity(abstractSet.TableType);
+            var navigationList = masterEntity.Navigations;
+            if (navigationList.Any())
+            {
+                provider.JoinList.AddRange(ExpressionExtension.Clone(navigationList));
+            }
+            // 查詢字段解析
+            StringBuilder selectSql = new StringBuilder("SELECT");
+            // 去重
+            if (abstractSet.IsDistinct)
+                selectSql.Append(" DISTINCT ");
+
+            // 不是自定義返回視圖則顯示所有字段
+            if (provider.Context.Set.SelectExpression == null)
+            {
+                var propertyBuilder = GetTableField(masterEntity);
+                selectSql.Append($" {propertyBuilder} ");
+            }
+            else // 自定義查詢字段
+            {
+                var selectExp = new SelectExpression(provider.Context.Set.SelectExpression, "", provider);
+                selectSql.Append($" {selectExp.SqlCmd} ");
+                provider.Params.AddDynamicParams(selectExp.Param);
+            }
+            return selectSql.ToString();
+        }
+
+
+        public override string ResolveSum(LambdaExpression selector)
+        {
+            if (selector == null)
+                throw new ArgumentException("selector");
+            var selectSql = "";
+
+            switch (selector.NodeType)
+            {
+                case ExpressionType.Lambda:
+                case ExpressionType.MemberAccess:
+                    {
+                        EntityObject entityObject = EntityCache.QueryEntity(selector.Parameters[0].Type);
+                        var memberName = selector.Body.GetCorrectPropertyName();
+                        string fieldName = $"{entityObject.AsName}.{providerOption.CombineFieldName(entityObject.FieldPairs[memberName])}";
+                        selectSql = $" SELECT IFNULL(SUM({fieldName}),0)  ";
+                    }
+                    break;
+                case ExpressionType.MemberInit:
+                    throw new DapperExtensionException("不支援該表達式類型");
+            }
+
+            return selectSql;
+        }
+
+        public override string ResolveMax(LambdaExpression selector)
+        {
+            if (selector == null)
+                throw new ArgumentException("selector");
+            var selectSql = "";
+
+            switch (selector.NodeType)
+            {
+                case ExpressionType.Lambda:
+                case ExpressionType.MemberAccess:
+                    {
+                        EntityObject entityObject = EntityCache.QueryEntity(selector.Parameters[0].Type);
+                        var memberName = selector.Body.GetCorrectPropertyName();
+                        string fieldName = $"{entityObject.AsName}.{providerOption.CombineFieldName(entityObject.FieldPairs[memberName])}";
+                        selectSql = $" SELECT Max({fieldName})  ";
+                    }
+                    break;
+                case ExpressionType.MemberInit:
+                    throw new DapperExtensionException("不支援該表達式類型");
+            }
+
+            return selectSql;
+        }
+
+        public override string ResolveMin(LambdaExpression selector)
+        {
+            if (selector == null)
+                throw new ArgumentException("selector");
+            var selectSql = "";
+
+            switch (selector.NodeType)
+            {
+                case ExpressionType.Lambda:
+                case ExpressionType.MemberAccess:
+                    {
+                        EntityObject entityObject = EntityCache.QueryEntity(selector.Parameters[0].Type);
+                        var memberName = selector.Body.GetCorrectPropertyName();
+                        string fieldName = $"{entityObject.AsName}.{providerOption.CombineFieldName(entityObject.FieldPairs[memberName])}";
+                        selectSql = $" SELECT Min({fieldName})  ";
+                    }
+                    break;
+                case ExpressionType.MemberInit:
+                    throw new DapperExtensionException("不支援該表達式類型");
+            }
+
+            return selectSql;
+        }
+
+        public override string ResolveWithNoLock(bool nolock)
+        {
+            return "";
+        }
+    }
+}
